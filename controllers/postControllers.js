f19a74819582b7b4d6b0bb0993787ac632a7e9b7
Burella Lucio <lucioburella@live.com>
2024-06-21 21:07:26 -0300

    Finalizando las rutas y los controladores

---
+++ controllers/postControllers.js
@@ -0,0 +1,8 @@
+const enviarPosts = () => {
+    console.log("esta funcion trae todo los posteos");
+}
+
+const enviarUnPost = () => {
+    console.log("esta funcion trae un  posteo");
+}
+module.exports = {enviarPosts,enviarUnPost}
