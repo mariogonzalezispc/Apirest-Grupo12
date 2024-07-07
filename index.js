71c7c96c70d273ecb109726ed41ccfd7c86e4933
Mario Gonzalez <mario.gonzalez@hotmail.com.ar>
2024-07-07 11:43:08 -0300

    Ajuste metyodos

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require("express")
-const cors = require("cors")
-const postRouter = require("./routes/postRouter.js")
-const app = express()
+const http = require ("http");
+const express = require ("express");
+const cors = require("cors");
+const postRouter = require("./routes/postRouter.js");
+const postRouter = require("./routes/getRouter.js");
+const postRouter = require("./routes/putRouter.js");
+const postRouter = require("./routes/deleteRouter.js");
+const app = express();
 const port = 3030
 
 app.use(cors())
