53356a691834ad8336ae98b41606df3ed2b080fe
Mario Gonzalez <mario.gonzalez@hotmail.com.ar>
2024-07-07 13:07:00 -0300

    Ajuste de index.js

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-const http = require ("http");
-const express = require ("express");
-const cors = require("cors");
-const postRouter = require("./routes/postRouter.js");
-const postRouter = require("./routes/getRouter.js");
-const postRouter = require("./routes/putRouter.js");
-const postRouter = require("./routes/deleteRouter.js");
+const express =require ('express');
 const app = express();
-const port = 3030
+const port = 3030;
 
-app.use(cors())
-app.use(express.json())
-
-app.get("/", (req, res) => {
-    res.send("estas en el home")
-})
-
-app.use("/posteos",postRouter)
-
-
-app.listen(port, () => {
-    console.log(`Servidor OK en el puerto ${port}`);
-})
+app.get("/", (req, res)=>
+)
