1e220fb375fcf039067b9bbb12486e858275b09f
Mario <mario.gonzalez@hotmail.com.ar>
2024-07-07 16:59:22 -0300

    ajuste

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require ("express");
 const cors = require ("cors");
 const Listado = require ("./routes/listado.js");
-const Nuevolibro = require ("./routes/crea.js");
-const Modifica = require ("./routes/modifica.js");
-const Borra = require ("./routes/borra.js");
+// const Nuevolibro = require ("./routes/crea.js");
+// const Modifica = require ("./routes/modifica.js");
+// const Borra = require ("./routes/borra.js");
 const app = express();
 const port = 3030;
 
@@ -19,10 +19,10 @@ app.get("/", (req, res)=>{
     res.send("Hola Mundo");
 })
 
-app.get("/crea", Nuevolibro);
+// app.get("/crea", Nuevolibro);
 app.get("/lista", Listado);
-app.get("/modifica", Modifica);
-app.get("/borra", Borra);
+// app.get("/modifica", Modifica);
+// app.get("/borra", Borra);
 
 
 
